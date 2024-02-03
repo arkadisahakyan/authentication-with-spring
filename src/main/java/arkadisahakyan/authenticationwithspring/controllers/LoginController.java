f5bef0ab33508ca314e0392503e5b32d738e79ca
Arkadi Sahakyan <arkadijsaakyan3@gmail.com>
2024-02-03 16:48:27 +0300

    Update

--- src/main/java/arkadisahakyan/authenticationwithspring/controllers/LoginController.java
+++ src/main/java/arkadisahakyan/authenticationwithspring/controllers/LoginController.java
@@ -1,23 +1,9 @@
 package arkadisahakyan.authenticationwithspring.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,30 +12,8 @@ public class LoginController {
 
     private static final Logger logger = LogManager.getLogger(LoginController.class);
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private SecurityContextRepository securityContextRepository;
-
     @GetMapping
     public String login() {
-        return "login";
-    }
-
-    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public String loginUser(@RequestBody MultiValueMap<String, String> formData, HttpServletRequest request, HttpServletResponse response, Model model) {
-        logger.info("LOGIN USER");
-        String username = formData.getFirst("username");
-        String password = formData.getFirst("password");
-        Authentication formAuth = new UsernamePasswordAuthenticationToken(username, password);
-        logger.info("AUTHENTICATE");
-        Authentication authentication = authenticationManager.authenticate(formAuth);
-        logger.info("AUTHENTICATED");
-        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
-        securityContext.setAuthentication(authentication);
-        SecurityContextHolder.setContext(securityContext);
-        securityContextRepository.saveContext(securityContext, request, response);
         return "login";
     }
 }
